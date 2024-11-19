1ae7a3ef5603002b7bc5d2cf69b3d30284df0038
Wallacy V. <wlcsv8080@outlook.com>
2024-11-19 07:33:40 -0300

    Decisões de arquitetura sempre são algo difícil

---
+++ core/elemento.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+class Elemento:
+    def __init__(self, linha_m: int, coluna_n: int, valor: Union[float, None):
+        self._linha_m = linha_m
+        self._coluna_n = coluna_n
+        self._valor = valor
+
+    def __str__(self):
+        return self._valor
+
--- core/matriz.py
+++ core/matriz.py
@@ -1,3 +1,20 @@
+from typing import List, Union
+import elemento
+
 class Matriz:
-    def __init__(self):
-        pass
+    def __init__(self, qtd_linhas_m: int, qtd_colunas_n: int):
+        self.qtd_linhas_m = qtd_linhas_m
+        self.qtd_colunas_n = qtd_colunas_n
+        self._elementos: List[Elemento] = []
+
+    def add_elemento(self, linha_m: int, coluna_n: int, valor: Union[float, None]) -> None:
+        if linha_m <= self.qtd_linhas_m:
+            if coluna_n <= self.qtd_colunas_n:
+                self._elementos.add(Elemento(linha_m, coluna_n, valor))
+            else:
+                raise ValueError("O valor fornecido para a coluna do elemento está fora dos limites da matriz")
+        else:
+            raise ValueError("O valor fornecido para linha do elemento está fora dos limites da matriz")
+
+
+
