e05fc8036774934b74ea16bf8fe021e713d4cc45
Wallacy V. <wlcsv8080@outlook.com>
2024-11-19 21:54:18 -0300

    Como eu ordeno isso?

--- core/matriz.py
+++ core/matriz.py
@@ -2,19 +2,17 @@ from typing import List, Union
 import elemento
 
 class Matriz:
-    def __init__(self, qtd_linhas_m: int, qtd_colunas_n: int):
+    def __init__(self, qtd_linhas_m: Union[int, None], qtd_colunas_n:Union[int, None]):
         self.qtd_linhas_m = qtd_linhas_m
         self.qtd_colunas_n = qtd_colunas_n
         self._elementos: List[Elemento] = []
 
+        self._linhas = []
+        self._colunas = []
+
     def add_elemento(self, linha_m: int, coluna_n: int, valor: Union[float, None]) -> None:
-        if linha_m <= self.qtd_linhas_m:
-            if coluna_n <= self.qtd_colunas_n:
-                self._elementos.add(Elemento(linha_m, coluna_n, valor))
-            else:
-                raise ValueError("O valor fornecido para a coluna do elemento está fora dos limites da matriz")
-        else:
-            raise ValueError("O valor fornecido para linha do elemento está fora dos limites da matriz")
+        pass
 
-
+    def del_elemento(self, linha_m: int, coluna_n: int) -> None:
+        pass
 
