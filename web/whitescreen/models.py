c5b3a6f760a6ce5ce788c69e0acafdf3aa377e38
Wallacy V. <wlcsv8080@outlook.com>
2024-11-24 20:20:40 -0300

    As vezes eu odeio o git

--- web/whitescreen/models.py
+++ web/whitescreen/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Matriz(models.Model):
+    matriz_nome = models.CharField(max_length=200)
+    qtd_linhas_m = models.IntegerField(default=0)
+    qtd_colunas_n = models.IntegerField(default=0)
+
+class Elemento(models.Model):
+    matriz = models.ForeignKey(Matriz, on_delete=models.CASCADE)
+    linha = models.IntegerField()
+    coluna = models.IntegerField()
+    valor = models.IntegerField()
